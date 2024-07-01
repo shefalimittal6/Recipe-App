07a85a8fe61adaa72293ffe75a15f9e4acfcd576
shefalimittal6 <mittal.shefali2000@gmail.com>
2024-07-01 21:24:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = '3dc2c8a806c74dedba6b6a10460fa7ff'
+BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    recipes = []
+    if request.method == 'POST':
+        query = request.form.get('query')
+        if query:
+            response = requests.get(BASE_URL, params={'query': query, 'apiKey': API_KEY})
+            if response.status_code == 200:
+                data = response.json()
+                recipes = data.get('results', [])
+    return render_template('index.html', recipes=recipes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
